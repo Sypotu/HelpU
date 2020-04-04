f810511006cf1689c2dd1b5c18b0a85394fee36d
Sylvain Potuaud <sylvain.potuaud@univrses.com>
2020-04-04 14:56:10 +0200

    add new task page

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => MyHomePage(),
-        '/task_detail': (context) => TaskDetailPage(ModalRoute.of(context).settings.arguments),
+        '/task_detail': (context) =>
+            TaskDetailPage(ModalRoute.of(context).settings.arguments),
+        '/new_task': (context) => CreateTaskPage(),
       },
     );
   }
@@ -30,12 +32,22 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(title: Text('COmunity')),
       body: _buildBody(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context,'/new_task');
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
     );
   }
 
   Widget _buildBody(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('task').orderBy('vote',descending: true).snapshots(),
+      stream: Firestore.instance
+          .collection('task')
+          .orderBy('vote', descending: true)
+          .snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
@@ -63,18 +75,16 @@ class _MyHomePageState extends State<MyHomePage> {
           borderRadius: BorderRadius.circular(5.0),
         ),
         child: ListTile(
-          title: Text(task.title),
-          trailing: Text(task.vote.toString()),
-          //onTap: () => task.reference.updateData({'vote': FieldValue.increment(1)}),
-          //onTap: () => task.reference.updateData({'vote': FieldValue.increment(1)}),
-          onTap: () {
-            Navigator.pushNamed(
-              context,
-              '/task_detail',
-              arguments: task,
-            );
-          }
-        ),
+            title: Text(task.title),
+            trailing: Text(task.vote.toString()),
+            //onTap: () => task.reference.updateData({'vote': FieldValue.increment(1)}),
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/task_detail',
+                arguments: task,
+              );
+            }),
       ),
     );
   }
@@ -83,17 +93,16 @@ class _MyHomePageState extends State<MyHomePage> {
 class TaskDetailPage extends StatelessWidget {
   final Task task;
 
-  TaskDetailPage (this.task);
+  TaskDetailPage(this.task);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(task.title),
       ),
       body: Center(
-        child:Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(task.title),
@@ -101,7 +110,66 @@ class TaskDetailPage extends StatelessWidget {
           ],
         ),
       ),
-      );
+    );
+  }
+}
+
+// Create a Form widget.
+class CreateTaskPage extends StatefulWidget {
+  @override
+  CreateTaskPageState createState() {
+    return CreateTaskPageState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class CreateTaskPageState extends State<CreateTaskPage> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+
+    return Scaffold(
+      appBar: AppBar(title: Text('COmunity')),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false
+                  // otherwise.
+                  if (_formKey.currentState.validate()) {
+                    // If the form is valid, display a Snackbar.
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
